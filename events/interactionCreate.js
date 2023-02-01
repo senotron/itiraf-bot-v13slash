63de230854a3501c83d98e45c91c10b68be0f945
Glitch (smooth-flannel-story) <none>
2023-02-01 15:26:35 +0000

    🌆✂️ Checkpoint ./slashKomutlar/g.js:54001497/197 ./events/interactionCreate.js:54001497/1285

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -113,48 +113,6 @@ module.exports = async (client, interaction, button) => {
 }} 
   
     
-        if (interaction.isButton()){
-
-      const {guild, member, customId, channel} = interaction;
-      const {butonrol1,buton1isim,butonrol2,buton2isim} = require("../psychoconf/config.json");
-
-
-      if(customId == "1buton"){
-
-       const role = interaction.guild.roles.cache.get(butonrol1)
-       const memberRole = interaction.member.roles;
-       
-       const hasRole = memberRole.cache.has(butonrol1);
-
-       if(hasRole){
-         memberRole.remove(butonrol1);
-         interaction.reply({content:`**${role}** rolü senden alındı`, ephemeral:true})
-       }else {
-        memberRole.add(butonrol1);
-        interaction.reply({content:`**${role}** rolü verildi`, ephemeral:true})
-       }
-     }
-          
-                if(customId == "2buton"){
-
-       const role = interaction.guild.roles.cache.get(butonrol2)
-       const memberRole = interaction.member.roles;
-       
-       const hasRole = memberRole.cache.has(butonrol2);
-
-       if(hasRole){
-         memberRole.remove(butonrol2);
-         interaction.reply({content:`**${role}** rolü senden alındı`, ephemeral:true})
-       }else {
-        memberRole.add(butonrol2);
-        interaction.reply({content:`**${role}** rolü verildi`, ephemeral:true})
-       }
-     }
-          
-          
-          
-          
-      }
   
   
   
