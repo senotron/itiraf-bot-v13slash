e9fdddad7a4e4898dc183d7df277b13f210f9912
senotron <105975100+senotron@users.noreply.github.com>
2025-01-04 12:10:07 +0400

    Update itirafadmin.js

--- letKomutlar/itirafadmin.js
+++ letKomutlar/itirafadmin.js
@@ -1,58 +1,92 @@
-const {Client, CommandInteraction, MessageEmbed, Permissions} = require("discord.js");
+const { Client, CommandInteraction, MessageEmbed, Permissions } = require("discord.js");
 const model = require("../models/guild");
+
 module.exports = {
-    name:"itiraf-admin-kanal",
-    description:"İtiraf Ayarları",
-    type:1,
-    options:[
-     
+  name: "confession-admin-channel",
+  description: "Confession Settings",
+  type: 1,
+  options: [
+    {
+      name: "set",
+      description: "Configuration Settings",
+      type: 1,
+      options: [
         {
-            name:"ayarla",
-            description:"Ayarlama İşlemleri",
-            type:1,
-            options:[{name:"itiraf_kanalı",description:"İtiraf kanalını ayarlar",type:7,required:true,channel_types:[0]}]            
+          name: "confession_channel",
+          description: "Sets the confession channel",
+          type: 7,
+          required: true,
+          channel_types: [0],
         },
-        {
-            name:"sıfırla",
-            description:"İtirafadmin kanalını sıfırlar",
-            type:1            
-        }
-    ],
-    /**
-     * 
-     * @param {Client} client 
-     * @param {CommandInteraction} interaction 
-     */
-    run: async (client, interaction) => {
-         const guild = interaction.guild;
+      ],
+    },
+    {
+      name: "reset",
+      description: "Resets the confession admin channel",
+      type: 1,
+    },
+  ],
+  run: async (client, interaction) => {
+    const guild = interaction.guild;
 
-        if(!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR))
-        {
-         // interaction.deferReply();
-          interaction.reply({content:"Bu komutu kullanabilmek için `Yönetici` yetkisine sahip olmalısın!",ephemeral:true});
-          return;
-        }
-        let SubCmd = interaction.options.getSubcommand();
-       // interaction.deferReply();
-        switch(SubCmd){
-        
-            case "ayarla":{
-          let {itiraf} = await model.findOne({GuildID:guild.id});
-        if(!itiraf) return interaction.reply({content: `İtiraf Sistemi Aktif Değil.`, ephemeral: true});
-                let itiraf_kanalı = interaction.options.getChannel("itiraf_kanalı");
-                await model.updateOne({GuildID: interaction.guild.id},{itirafadminChannel: itiraf_kanalı.id},{upsert:true});
-                interaction.reply({embeds:[new MessageEmbed().setTitle("İtiraf Kanalı Ayarlandı!").setColor("GREEN").setDescription(`İtiraf kanalı ayarlandı! İtiraf kanalınız: <#${itiraf_kanalı.id}>`)]});
-                break;
-            }
-            case "sıfırla":{
-                        let {itiraf} = await model.findOne({GuildID:guild.id});
-        if(!itiraf) return interaction.reply({content: `İtiraf Sistemi Aktif Değil.`, ephemeral: true});
-                await model.updateOne({GuildID: interaction.guild.id},{itirafadminChannel: null},{upsert:true});
-                interaction.reply({embeds:[new MessageEmbed().setTitle("İtiraf Kanalı Sıfırlandı!").setColor("RED").setDescription(`İtiraf kanalı kapatıldı! Artık sunucunuzda itirafların gideceği kanalı yok!`)]});
-                break;
-            }
-        }
-
-
+    if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      interaction.reply({
+        content: "You must have `Administrator` permissions to use this command!",
+        ephemeral: true,
+      });
+      return;
     }
-}
+
+    const subCommand = interaction.options.getSubcommand();
+
+    switch (subCommand) {
+      case "set": {
+        const { confession } = await model.findOne({ GuildID: guild.id }) || {};
+        if (!confession)
+          return interaction.reply({
+            content: "The Confession System is not active.",
+            ephemeral: true,
+          });
+
+        const confessionChannel = interaction.options.getChannel("confession_channel");
+        await model.updateOne(
+          { GuildID: interaction.guild.id },
+          { confessionAdminChannel: confessionChannel.id },
+          { upsert: true }
+        );
+        interaction.reply({
+          embeds: [
+            new MessageEmbed()
+              .setTitle("Confession Channel Set!")
+              .setColor("GREEN")
+              .setDescription(`The confession channel has been set! Channel: <#${confessionChannel.id}>`),
+          ],
+        });
+        break;
+      }
+      case "reset": {
+        const { confession } = await model.findOne({ GuildID: guild.id }) || {};
+        if (!confession)
+          return interaction.reply({
+            content: "The Confession System is not active.",
+            ephemeral: true,
+          });
+
+        await model.updateOne(
+          { GuildID: interaction.guild.id },
+          { confessionAdminChannel: null },
+          { upsert: true }
+        );
+        interaction.reply({
+          embeds: [
+            new MessageEmbed()
+              .setTitle("Confession Channel Reset!")
+              .setColor("RED")
+              .setDescription("The confession channel has been removed! There is no confession channel in your server now."),
+          ],
+        });
+        break;
+      }
+    }
+  },
+};
